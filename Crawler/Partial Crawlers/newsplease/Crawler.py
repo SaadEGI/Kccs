205c13a9690ef51a59334a87ce54af13d14b6735
SufyanDahalan <sufyan.dahalan@gmail.com>
2021-05-30 22:38:46 +0200

    First of two iterations on the news crawler. Second iteration will enable passing parameters to google search

---
+++ Crawler/Partial Crawlers/newsplease/Crawler.py
@@ -0,0 +1,38 @@
+from newsfetch.google import google_search
+from newsfetch.news import newspaper
+import os
+import time
+import json
+
+index = 0
+websiteList = []
+data = {}
+
+with open('WebsiteLists') as f:
+        websiteList = [line.strip() for line in f]
+
+for website in websiteList:
+    query = google_search('woman',website)
+    print(query.urls)
+    for url in query.urls:
+        article = newspaper(url)
+        data[index] = {
+            'Title': article.headline,
+            'Website': article.source_domain,
+            'Description': article.description,
+            'Main Text': article.article,
+            'URL': article.uri
+        }
+        index = index + 1
+
+
+if(not os.path.exists('./CrawledData/')):
+    os.mkdir('./CrawledData')
+
+filename = f'{time.strftime("%Y%m%d-%H%M%S")}_raw.json'
+
+with open(f'./CrawledData/{filename}', "w") as fp:
+    json.dump(data, fp, indent = 4)
+
+
+# https://www.google.com/search?q={{seach query}}&tbs=qdr:m
