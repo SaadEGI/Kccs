25187290895be1430ac868ce8b95138e77b5473f
SaadEGI <christrophe.stephane@gmail.com>
2021-06-10 15:24:18 +0200

    Twitter Crawler update

--- Crawlers/Twitter/hashtagCrawler.py
+++ Crawlers/Twitter/hashtagCrawler.py
@@ -1,42 +1,47 @@
 import tweepy
 import json
+import sys
 
-tweetsPerQry = 100
-# test
-maxTweets = 100
-hashtag = "#corona"  # test
-usertweets = {}
-a = 0
-##Private, please do not share these codes with anyone, thanks
-authentication = tweepy.OAuthHandler('9Fmf0uyQ4kAlgfqLTRCgmvAXS', 'ASKvsmRRf4sVfv06y12H2VMKT4m3iarQUDuRnbcSRIDiVIg5Ha')
-authentication.set_access_token('2993256339-ptMVZ0jCyGZWJfE9u7hnulkG1s0OlmbT1wMJ06l',
-                                'x5p9NYlH6B7QNXWiB2OB4DyqmqW3gQePrjUWoIzxIet4q')
+authentication = tweepy.OAuthHandler('ColqKqV1rfcpd1ytv9RMS37eO', 'otDk7hQGQebS1csNQ5K6aaEqRBHjatGNJmLWw4W31rumCK6ak7')
+authentication.set_access_token('1014769894276128769-U4Vl2WwuqivDYqImj7TgZYentFMNfJ',
+                                    'lIXJ2do4p5W3sFmqokB2O8IYFr8o7kZjNh4l7IKxBK0kv')
 api = tweepy.API(authentication, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-maxId = -1
-tweetCount = 0
+
+
+def get_tweets(hashtag, usertweets):
+
+    tweetsPerQry = 100
+
+    maxTweets = 100
+
+    maxId = -1
+    tweetCount = 0
+
+    while tweetCount < maxTweets:
+        if (maxId <= 0):
+            newTweets = api.search(q=hashtag, count=tweetsPerQry, result_type="recent", tweet_mode="extended")
+        else:
+            newTweets = api.search(q=hashtag, count=tweetsPerQry, max_id=str(maxId - 1), result_type="recent",
+                                   tweet_mode="extended")
+
+        if not newTweets:
+            print("End")
+            break
+
+        for tweet in newTweets:
+            usertweets[tweet.full_text] = None
+
+        tweetCount += len(newTweets)
+        maxId = newTweets[-1].id
+
+
+usertweets = {}
 file_path = sys.argv[1]
+with open(r"Hashtags.txt") as f:
+    lines = f.read().splitlines()
+for li in lines:
+    usertweets[get_tweets(li, usertweets)] = None
 
-
-# possibilty to add hashtags list here
-while tweetCount < maxTweets:
-    if (maxId <= 0):
-        newTweets = api.search(q=hashtag, count=tweetsPerQry, result_type="recent", tweet_mode="extended")
-    else:
-        newTweets = api.search(q=hashtag, count=tweetsPerQry, max_id=str(maxId - 1), result_type="recent",
-                               tweet_mode="extended")
-
-    if not newTweets:
-        print("End")
-        break
-
-    for tweet in newTweets:
-        usertweets[tweet.full_text] = None
-
-    tweetCount += len(newTweets)
-    maxId = newTweets[-1].id
-
-with open(rf'{file_path+"/Newspapers_raw.json"}', 'w', encoding='utf-8') as f:
-    json.dump(usertweets, f, ensure_ascii=False, indent=4)
-
-
+with open(rf'{file_path+"/twitterhashtagstweets.json"}', 'w', encoding='utf-8') as f:
+        json.dump(usertweets, f, ensure_ascii=False, indent=4)
 
--- Crawlers/Twitter/usertweetsCrawler.py
+++ Crawlers/Twitter/usertweetsCrawler.py
@@ -4,10 +4,10 @@ import datetime, time
 import json
 
 ##Private, please do not share these codes with anyone, thanks
-authentication = tweepy.OAuthHandler('9Fmf0uyQ4kAlgfqLTRCgmvAXS',
-                                    'ASKvsmRRf4sVfv06y12H2VMKT4m3iarQUDuRnbcSRIDiVIg5Ha')
-authentication.set_access_token('2993256339-ptMVZ0jCyGZWJfE9u7hnulkG1s0OlmbT1wMJ06l',
-                                'x5p9NYlH6B7QNXWiB2OB4DyqmqW3gQePrjUWoIzxIet4q')
+authentication = tweepy.OAuthHandler('ColqKqV1rfcpd1ytv9RMS37eO', 'otDk7hQGQebS1csNQ5K6aaEqRBHjatGNJmLWw4W31rumCK6ak7')
+authentication.set_access_token('1014769894276128769-U4Vl2WwuqivDYqImj7TgZYentFMNfJ',
+                                    'lIXJ2do4p5W3sFmqokB2O8IYFr8o7kZjNh4l7IKxBK0kv')
+
 api = tweepy.API(authentication)
 
 file_path = sys.argv[1]
@@ -36,7 +36,7 @@ with open(r"Twitterlist") as f:
 for li in lines:
     usertweets[get_tweets(api, li)] = None
 
-with open(rf'{file_path+"UserTweets.json"}', 'w') as file:
-    json.dump(usertweets, file, indent = 4)
+with open('usertweets.json', 'w', encoding='utf-8') as f:
+    json.dump(usertweets, f, ensure_ascii=False, indent=4)
 
 
