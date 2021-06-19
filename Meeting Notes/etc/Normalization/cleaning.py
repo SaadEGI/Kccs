c2f5b9b3c4907994a19d49eb0a472f4f56569fac
SufyanDahalan <sufyan.dahalan@gmail.com>
2021-06-19 11:45:30 +0200

    Added stemmer and lemmatizer

--- Meeting Notes/etc/Normalization/cleaning.py
+++ Meeting Notes/etc/Normalization/cleaning.py
@@ -2,6 +2,29 @@ import string
 import re
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from nltk.stem import SnowballStemmer
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+
+def stemSentence(sentence):
+    token_words = word_tokenize(sentence)
+    stem_sentence = []
+    stemmer = SnowballStemmer("english")
+
+    for word in token_words:
+        stem_sentence.append(stemmer.stem(word))
+    return " ".join(stem_sentence)
+
+
+def lemmSentence(sentence):
+    lemma_sentence=[]
+    lemmatizer = WordNetLemmatizer()
+
+    for word in sentence:
+        lemma_sentence.append(lemmatizer.lemmatize(word))
+    return "".join(lemma_sentence)
+
 
 input_str = "\t \nThis &is [an] 2 33 example? {of} string. with.? 45 4 punctuation!!!!" # Sample string
 
@@ -23,4 +46,8 @@ stop_words = set(stopwords.words('english'))
 
 tokens = word_tokenize(result)
 result = [i for i in tokens if not i in stop_words]
+result =  " ".join(result)
 print (result)
+
+print(stemSentence(result))
+
