69904b406846e44c0e419b6938d84292c93307c6
SufyanDahalan <sufyan.dahalan@gmail.com>
2021-06-25 00:45:23 +0200

    added count vectorization

--- Meeting Notes/etc/Normalization/cleaning.py
+++ Meeting Notes/etc/Normalization/cleaning.py
@@ -7,7 +7,7 @@ from nltk.stem import SnowballStemmer
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import sent_tokenize, word_tokenize
 from newsplease import NewsPlease
-
+from sklearn.feature_extraction.text import CountVectorizer
 
 def stemSentence(sentence):
     token_words = word_tokenize(sentence)
@@ -18,7 +18,6 @@ def stemSentence(sentence):
         stem_sentence.append(stemmer.stem(word))
     return " ".join(stem_sentence)
 
-
 def lemmSentence(sentence):
     lemma_sentence=[]
     lemmatizer = WordNetLemmatizer()
@@ -27,42 +26,35 @@ def lemmSentence(sentence):
         lemma_sentence.append(lemmatizer.lemmatize(word))
     return "".join(lemma_sentence)
 
-
-# input_str = "\t \nThis &is [an] 2 33 example? {of} string. with.? 45 4 punctuation!!!!" # Sample string
-
-# article = NewsPlease.from_url('https://www.nytimes.com/2021/06/19/world/americas/brazil-drought.html')
-# # make a dictonary object from NewsPlease object
-# d = article.__dict__
-
-# # Convert it to string 
-# s = json.dumps(d, indent=4, sort_keys=True, default=str)
-
-# input_str = article.maintext
-
-# # write it to json file
-# with open('new.json', "w") as fp:
-# 	fp.write(s)
-
-with open('new.json') as json_file:
-    data = json.load(json_file)
-input_str = data["maintext"]
+def normatizeDocument(sentence):
+    sentence = re.sub(r"\d+", "", sentence)
+    # Remove punctuations, instead put spaces
+    result = sentence.translate(str.maketrans("", "", string.punctuation))
+    #cleaning up
+    result = re.sub(' +', ' ', result)
+    # remove whitespaces (tabs,..)
+    result = result.strip()
 
 
-# Replace digits with space
-input_str = re.sub(r"\d+", "", input_str)
-# Remove punctuations, instead put spaces
-result = input_str.translate(str.maketrans("", "", string.punctuation))
-#cleaning up
-result = re.sub(' +', ' ', result)
-# remove whitespaces (tabs,..)
-result = result.strip()
+    stop_words = set(stopwords.words('english'))
 
-stop_words = set(stopwords.words('english'))
+    tokens = word_tokenize(result)
+    result = [i for i in tokens if not i in stop_words]
 
-tokens = word_tokenize(result)
-result = [i for i in tokens if not i in stop_words]
+    result =  " ".join(result)
+    return result
 
-print (result)
-result =  " ".join(result) 
+def createWordVector(articles): # takes a list containing all articles to be vectorized as an argument
+    # Technique is called count vectorization
+    vectorizer = CountVectorizer()
+    wordVectors = vectorizer.fit_transform(articles)
+    return wordVectors.toarray()
 
-print(stemSentence(result))
+
+document = "\t \nThis &is [an] 2 33 example? {of} string. with.? 45 4 punctuation!!!!" # Sample string
+
+print(document)
+result = stemSentence(normatizeDocument(document))
+print(result)
+result = createWordVector([result])
+print(result)
