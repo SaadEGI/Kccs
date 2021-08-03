21532731481e785db74861ca70d2d58d3189c1e0
ASUS <saad.bendou@uni-wuppertal.de>
2021-08-03 18:53:10 +0200

    IDF/TF

--- Crawlers/Twitter/hashtagCrawler.py
+++ Crawlers/Twitter/hashtagCrawler.py
@@ -1,6 +1,7 @@
 import tweepy
 import json
 import sys
+import writer from csv
 
 authentication = tweepy.OAuthHandler('ColqKqV1rfcpd1ytv9RMS37eO', 'otDk7hQGQebS1csNQ5K6aaEqRBHjatGNJmLWw4W31rumCK6ak7')
 authentication.set_access_token('1014769894276128769-U4Vl2WwuqivDYqImj7TgZYentFMNfJ',
@@ -8,7 +9,8 @@ authentication.set_access_token('1014769894276128769-U4Vl2WwuqivDYqImj7TgZYentFM
 api = tweepy.API(authentication, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 
-def get_tweets(hashtag, usertweets):
+def get_tweets(hashtag, listoflists):
+
 
     tweetsPerQry = 100
 
@@ -29,18 +31,23 @@ def get_tweets(hashtag, usertweets):
             break
 
         for tweet in newTweets:
-            usertweets[tweet.full_text] = None
+            List = [hashtag, tweet.user.name, tweet.full_text, tweet.created_at, tweet.user.location]
+            listoflists.append(List)
 
         tweetCount += len(newTweets)
         maxId = newTweets[-1].id
 
+header = ['Hashtag', 'username', 'fulltext', 'Date', 'Location']
+listoflists = []
 
-usertweets = {}
-file_path = sys.argv[1]
 with open(r"Hashtags.txt") as f:
     lines = f.read().splitlines()
 for li in lines:
-    usertweets[get_tweets(li, usertweets)] = None
+    get_tweets(li,listoflists)
+
+    with open("out.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows()
 
 with open(rf'{file_path+"/twitterhashtagstweets.json"}', 'w', encoding='utf-8') as f:
         json.dump(usertweets, f, ensure_ascii=False, indent=4)
